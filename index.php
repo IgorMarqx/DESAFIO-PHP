6b0c084c1eb20749c3229664740fdcdde2868cf4
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-28 11:25:45 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <form action="" class="form-controll">
+        <div class="div-controll">
+            <div class="input-controll">
+                <label for="">Nome</label>
+                <input type="text">
+            </div>
+
+            <div class="input-controll">
+                <label for="">E-mail</label>
+                <input type="text">
+            </div>
+
+            <div class="btn-controll">
+                <input class="btn" type="button" value="Enviar">
+            </div>
+        </div>
+    </form>
+
+</body>
+
+</html>
