2f37030dfb4e13c09758b5f75821021b2c3d290c
IgorMarqx <igormarquesdeazevedo11@gmail.com>
2023-07-28 12:24:45 -0300

    feat: create form

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,20 +14,34 @@
 
 <body>
 
-    <form action="" class="form-controll">
+    <form action="create.php" method="POST" class="form-controll">
         <div class="div-controll">
             <div class="input-controll">
-                <label for="">Nome</label>
-                <input type="text">
+                <label for="name">Nome</label>
+                <input type="text" name="name" value="<?php if(isset($_POST['name'])) echo $_POST['name']; ?>">
+
+                <?php
+                if (isset($_SESSION['name'])) {
+                    echo $_SESSION['name'];
+                    unset($_SESSION['name']);
+                }
+                ?>
             </div>
 
             <div class="input-controll">
-                <label for="">E-mail</label>
-                <input type="text">
+                <label for="email">E-mail</label>
+                <input type="text" name="email">
+
+                <?php
+                if (isset($_SESSION['email'])) {
+                    echo $_SESSION['email'];
+                    unset($_SESSION['email']);
+                }
+                ?>
             </div>
 
             <div class="btn-controll">
-                <input class="btn" type="button" value="Enviar">
+                <input class="btn" type="submit" value="Enviar">
             </div>
         </div>
     </form>
